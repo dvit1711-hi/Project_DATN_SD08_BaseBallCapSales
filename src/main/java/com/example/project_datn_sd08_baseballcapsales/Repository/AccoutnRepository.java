c392d206d91558bb2877c8d895b4abbeb3ac98d7
dvit1711-hi <vietnd1711@gmai.com>
2026-01-22 20:59:11 +0700

    Repository Account thieu roled

---
+++ src/main/java/com/example/project_datn_sd08_baseballcapsales/Repository/AccoutnRepository.java
@@ -0,0 +1,7 @@
+package com.example.project_datn_sd08_baseballcapsales.Repository;
+
+import com.example.project_datn_sd08_baseballcapsales.Model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccoutnRepository extends JpaRepository<Account, Integer> {
+}
