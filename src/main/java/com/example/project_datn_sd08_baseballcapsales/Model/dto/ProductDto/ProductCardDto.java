2fb1c3303cafaa337891fbb37e73f022365aa044
dvit1711-hi <vietnd1711@gmai.com>
2026-04-03 10:14:17 +0700

    bo loc tim kiem

--- src/main/java/com/example/project_datn_sd08_baseballcapsales/Model/dto/ProductDto/ProductCardDto.java
+++ src/main/java/com/example/project_datn_sd08_baseballcapsales/Model/dto/ProductDto/ProductCardDto.java
@@ -13,25 +13,54 @@ public class ProductCardDto {
 
     private Integer productID;
     private Integer productColorID;
+
     private String productName;
     private BigDecimal price;
+    private String status;
+
+    private Integer brandID;
+    private String brandName;
+
+    private Integer materialID;
+    private String materialName;
+
+    private Integer colorID;
     private String colorName;
     private String colorCode;
+
+    private Integer sizeID;
     private String sizeName;
+
     private String mainImage;
-    private String status;
+    private Integer stockQuantity;
 
     public ProductCardDto(ProductColor pc) {
         this.productID = pc.getProductID().getId();
         this.productColorID = pc.getId();
+
         this.productName = pc.getProductID().getProductName();
         this.price = pc.getPrice();
         this.status = pc.getProductID().getStatus();
+        this.stockQuantity = pc.getStockQuantity();
 
-        this.colorName = pc.getColorID().getColorName();
-        this.colorCode = pc.getColorID().getColorCode();
+        if (pc.getProductID().getBrandID() != null) {
+            this.brandID = pc.getProductID().getBrandID().getBrandID();
+            this.brandName = pc.getProductID().getBrandID().getName();
+        }
+
+        if (pc.getProductID().getMaterialID() != null) {
+            this.materialID = pc.getProductID().getMaterialID().getMaterialID();
+            this.materialName = pc.getProductID().getMaterialID().getMaterialName();
+        }
+
+        if (pc.getColorID() != null) {
+            this.colorID = pc.getColorID().getId(); // nếu entity Color dùng getColorID() thì đổi lại
+            this.colorName = pc.getColorID().getColorName();
+            this.colorCode = pc.getColorID().getColorCode();
+        }
 
         if (pc.getSizeID() != null) {
+            this.sizeID = pc.getSizeID().getSizeID(); // nếu SizeEntity dùng getId() thì đổi lại
             this.sizeName = pc.getSizeID().getSizeName();
         }
 
