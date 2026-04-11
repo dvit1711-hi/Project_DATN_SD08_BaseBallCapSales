85b54197882588c8cde6a1cfc45bfffa7d87ff04
dvit1711-hi <vietnd1711@gmai.com>
2026-04-11 16:16:52 +0700

    quanlybienthe quanlysanpham fix List<string> => Lisgt<ImageDto>

--- src/main/java/com/example/project_datn_sd08_baseballcapsales/Model/dto/ProductDto/ImageDto.java
+++ src/main/java/com/example/project_datn_sd08_baseballcapsales/Model/dto/ProductDto/ImageDto.java
@@ -7,10 +7,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ImageDto {
+    private Integer imageID;
     private String imageUrl;
     private Boolean isMain;
 
     public ImageDto(Image image) {
+        this.imageID = image.getId();
         this.imageUrl = image.getImageUrl();
         this.isMain = image.getIsMain();
     }
